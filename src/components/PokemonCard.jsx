42417b8e7ccb7b7e496db9d554e890733ea61f37
christophe Dubois <dubois51ch@gmail.com>
2023-03-28 10:22:45 +0200

    bulbisar

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,11 @@
+function PokemonCards() {
+    return (
+    <figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+             alt="bulbasaur pokemon" />
+        <figcaption>bulbasaur</figcaption>
+    </figure>
+    )
+}
+
+export default PokemonCards
