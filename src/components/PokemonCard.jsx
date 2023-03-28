43903f9c6c62c2950f5bf25b5d85cd724e16b71c
christophe Dubois <dubois51ch@gmail.com>
2023-03-28 12:06:50 +0200

    Exo 2 pokemonList

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,26 @@
-function PokemonCards() {
-    return (
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+function PokemonCard() {
+  const pokemon = pokemonList[0];
+
+  return (
     <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-             alt="bulbasaur pokemon" />
-        <figcaption>bulbasaur</figcaption>
+      {pokemon.imgSrc != null ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
-    )
+  );
 }
 
-export default PokemonCards
+export default PokemonCard;
