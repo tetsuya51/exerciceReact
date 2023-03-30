603503e2c852e5f658fbe5fe17d42b6c193be3f8
christophe Dubois <dubois51ch@gmail.com>
2023-03-30 10:54:54 +0200

    exo State 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
 
 const pokemonList = [
   {
@@ -7,14 +8,47 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const pastClick = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const nextClick = () => {
+    if (count < pokemonList.length-1) {
+      setCount(count + 1);
+    }
+  };
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[count]} />
+      <button onClick={pastClick}>Précédent</button>
+      <button onClick={nextClick}>Suivant</button>
+      <p>
+        {count+1}/{pokemonList.length}
+      </p>
     </div>
   );
 }
